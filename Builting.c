19769c61788f4534391f5748679230b572edf816
Ele4327 <3380@holbertonschool.com>
2021-08-24 18:17:54 -0500

    Built-In

---
+++ Builting.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+
+/**
+ * check_builder - Builder Function Checker
+ * @command: Line of the commmand to check.
+ * Return: 0 Succes -1 fail
+*/
+
+int check_builder(char **command)
+{
+	const_t func[] = {
+	    {"cd", NULL},
+	    {"env", NULL},
+	    {"help", NULL},
+	    {"echo", NULL},
+	    {"history", NULL},
+	    {NULL, NULL}};
+
+	int _check = 0;
+
+	if (*command == NULL)
+		return (-1);
+	while ((func + _check)->command)
+	{
+		if (_strcmp(command[0], (func + _check)->command) == 0)
+			return (0);
+		_check++;
+	}
+	return (-1);
+}
