c9fa1251733877cea0eb2ee6bfb7817e83dfaea6
Ele4327 <3380@holbertonschool.com>
2021-08-24 18:18:04 -0500

    Check CMD

---
+++ check_cmd.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+
+/**
+ * check_command - Running simple commands of the shell
+ * @command: line of the command tokenizer
+ * @input: User Input
+ * @counter: Excecution Time in case of command not found
+ * @argv: Name Of The Program
+ * Return: 1 in Case this command is NULL or 0 wrong command Execute
+*/
+
+int check_command(char **command, char *input, int counter, char **argv)
+{
+	int status;
+	pid_t pid;
+
+	if (*command == NULL)
+		return (-1);
+	pid = pids_Son(*command);
+	if (pid == 0)
+	{
+		if (strncmp(*command, "./", 2) != 0 && strncmp(*command, "/", 1) != 0)
+			path(command);
+
+		if (execve(*command, command, environ) == -1)
+		{
+			errors_print(command[0], counter, argv);
+			free(input);
+			free(command);
+			return (EXIT_FAILURE);
+		}
+		return (EXIT_SUCCESS);
+	}
+	wait(&status);
+	return (0);
+}
