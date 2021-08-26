14bbf9b5ba61ffbd7cdd7874c727383e07039ef3
Ele4327 <3380@holbertonschool.com>
2021-08-25 23:24:59 -0500

    Char Function

---
+++ Char_Func2.c
@@ -0,0 +1,58 @@
+#include "shell.h"
+/**
+ * _atoi - convert to a int
+ * @s:string
+ * Return:int
+ */
+int _atoi(char *s)
+{
+	int i, j, n, x;
+
+	i = n = 0;
+	x = 1;
+	while ((s[i] < '0' || s[i] > '9') && (s[i] != '\0'))
+	{
+		if (s[i] == '-')
+			x *= -1;
+		i++;
+	}
+	j = i;
+	while ((s[j] >= '0') && (s[j] <= '9'))
+	{
+		n = (n * 10) + x * ((s[j]) - '0');
+		j++;
+	}
+	return (n);
+}
+/**
+ * _puts - print a string
+ * @str:pointer char
+ * return:void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - lenght of string
+ * @s:char
+ * Return:int
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	return (i);
+}
